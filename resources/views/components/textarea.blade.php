33819fab953b33d16fd08c003355cf9471112e91
Pedro Henrique Volpatto <106243882+pedroquack@users.noreply.github.com>
2024-08-15 17:58:58 -0300

    Update textarea.blade.php

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col {{$class}}">
     <label for="{{$id}}" class="font-bold">{{$label}}</label>
     <textarea id="{{$id}}" name="{{$name}}" placeholder="{{$placeholder}}"
-        class="rounded-lg text-black resize-none h-28 @if($errors->has($name)) input-error @endif"></textarea>
+        class="rounded-lg text-black resize-none h-28 @if($errors->has($name)) input-error @endif">{{old($name)}}</textarea>
     @if ($errors->has($name))
     <span class="invalid-feedback">
         <p>{{ $errors->first($name)}}</p>
